f33fed96b4905a809d16f15b37805322d161d3ef
ICEI30Y <petchtone@gmail.com>
2019-05-20 20:47:24 +0700

    Create GameBoard.java
    GameBoard class frame success

---
+++ GameBoard.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class GameBoard
+{
+    private ArrayList<Square> squareGrid;
+    
+    private ArrayList<Square> currentPlacement;
+
+    public GameBoard()
+    {
+
+    }
+
+    private void initialize()
+    {
+
+    }
+
+    public void viewBoard()
+    {
+
+    }
+
+    public boolean placeTileTo(int positionX,int positionY,Tile tile)
+    {
+
+    }
+
+    public void clearBoard()
+    {
+
+    }
+
+    public int calculateLastPlacementScore()
+    {
+
+    }
+
+    public void restoreTileToPlayer()
+    {
+
+    }
+
+    public boolean removeTileFromBoard(int tileID)
+    {
+        
+    }
+
+}
