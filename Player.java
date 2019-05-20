ee89f91b9fe48af47e5d87bbd3bbe5b72e2c2fb7
ICEI30Y <petchtone@gmail.com>
2019-05-20 20:35:52 +0700

    Update Player.java
    Player Class Frame success

--- Player.java
+++ Player.java
@@ -5,15 +5,60 @@ public class Player
     private TileCollection playerTiles;
     private int score;
     
-    /*
-    * 
-    *
-    */
+    
     public int getPlayerID()
     {
 
     }
 
+    public String getPlayerName()
+    {
 
+    }
+
+    public int getScore()
+    {
+
+    }
+
+    public void updateScore(int score)
+    {
+
+    }
+
+    public void clearScore()
+    {
+
+    }
+
+    public boolean addTile(Tile tile)
+    {
+
+    }
+
+    public int getPlayerTilesCount()
+    {
+
+    }
+
+    public Tile getTile(int tileID)
+    {
+
+    }
+
+    public Tile getStartTile()
+    {
+
+    }
+
+    public void selectTiles(int numberOfTile)
+    {
+
+    }
+
+    public void swapTile(int[] tilesID)
+    {
+
+    }
 
 }
