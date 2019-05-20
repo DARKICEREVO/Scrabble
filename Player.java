f99218d958dd6e4806a874ce2bd07ba6a79344a1
ICEI30Y <petchtone@gmail.com>
2019-05-20 20:05:47 +0700

    Create Player.java
    Player Class (Frame)

---
+++ Player.java
@@ -0,0 +1,19 @@
+public class Player
+{
+    private int playerID;
+    private String playerName;
+    private TileCollection playerTiles;
+    private int score;
+    
+    /*
+    * 
+    *
+    */
+    public int getPlayerID()
+    {
+
+    }
+
+
+
+}
