63b13764359fae4bc447d5c440f3775935fb4b22
ICEI30Y <petchtone@gmail.com>
2019-05-23 02:04:32 +0700

    Update GameBoard.java
    Game board
    + swap x and y
    + new validation
    +new get square method

--- GameBoard.java
+++ GameBoard.java
@@ -47,7 +47,7 @@ public class GameBoard
     }
     private static String getSquareString(int positionX,int positionY)
     {
-        int index = (positionX-1)*15+(positionY-1);
+        int index = (positionY-1)*15+(positionX-1);
         Square square = squareGrid.get(index);
         int letterMultiplier = square.getLetterMultiplier();
         int wordMultiplier = square.getWordMultiplier();
@@ -93,6 +93,19 @@ public class GameBoard
             thingOnSquare = tile.getTileLetter();
         }
         return thingOnSquare;
+    }
+    public static Square getSquare(int positionX,int positionY)
+    {
+        Square square;
+        if(positionX>=1 && positionX<=15 && positionY>=1 && positionY<=15)
+        {
+            square = squareGrid.get((positionY-1)*15+(positionX-1));
+            return square;
+        }
+        else
+        {
+            return null;
+        }
     }
     /**
      * show square layout and every tile on the board
@@ -144,10 +157,10 @@ public class GameBoard
      */
     public static boolean placeTileTo(int positionX,int positionY,Tile tile)
     {
-        if(squareGrid.get((positionX-1)*15+(positionY-1)).isEmpty())
+        if(getSquare(positionX, positionY).isEmpty())
         {
-            squareGrid.get((positionX-1)*15+(positionY-1)).setTileOnSquare(tile);
-            currentPlacement.add(squareGrid.get((positionX-1)*15+(positionY-1)));
+            getSquare(positionX, positionY).setTileOnSquare(tile);
+            currentPlacement.add(getSquare(positionX, positionY));
             return true;
         }
         else
@@ -192,13 +205,24 @@ public class GameBoard
         Square targetSquare;
         boolean isAlign=true;
         boolean alignX=true,alignY=true;
+        boolean flagX = false,flagY = false;
         int mainX,mainY;
+        int lowestX = Integer.MAX_VALUE,lowestY = Integer.MAX_VALUE;
+        
         mainX = currentPlacement.get(0).getPositionX();
         mainY = currentPlacement.get(0).getPositionY();
         while(iterator.hasNext())
         {
             targetSquare = iterator.next();
-            if(targetSquare.getPositionX()==mainX)
+            if(targetSquare.getPositionX() < lowestX)
+            {
+                lowestX = targetSquare.getPositionX();
+            }
+            if(targetSquare.getPositionY() < lowestY)
+            {
+                lowestY = targetSquare.getPositionY();
+            }
+            if(targetSquare.getPositionX() == mainX)
             {
                 alignX = alignX && true;
             }
@@ -206,7 +230,7 @@ public class GameBoard
             {
                 alignX = alignX && false;
             }
-            if(targetSquare.getPositionY()==mainY)
+            if(targetSquare.getPositionY() == mainY)
             {
                 alignY = alignY && true;
             }
@@ -216,9 +240,83 @@ public class GameBoard
             }
         }
         isAlign = alignX || alignY;
-        if(isAlign)
+        if(isAlign && alignX)
         {
-            return true;
+            int checkerY = lowestY;
+            int count=0;
+            ArrayList<Square> aroundSquare = new ArrayList<Square>();
+
+            while(getSquare(mainX,checkerY)!=null)
+            {
+                if(currentPlacement.contains(getSquare(mainX,checkerY)))
+                {
+                    count++;
+                    aroundSquare.add(getSquare(mainX, checkerY - 1));
+                    aroundSquare.add(getSquare(mainX, checkerY + 1));
+                    aroundSquare.add(getSquare(mainX - 1, checkerY));
+                    aroundSquare.add(getSquare(mainX + 1, checkerY));
+                    for(int i=0;i<aroundSquare.size();i++)
+                    {
+                        if(aroundSquare.get(i)!=null)
+                        {
+                            if(!currentPlacement.contains(aroundSquare.get(i)))
+                            {
+                                flagX = flagX || true;
+                            }
+                        }
+                        
+                    }
+                }
+                checkerY++;
+            }
+            if(count != currentPlacement.size() || !flagX)
+            {
+                restoreTileToPlayer();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (isAlign && alignY)
+        {
+            int checkerX = lowestX;
+            int count=0;
+            ArrayList<Square> aroundSquare = new ArrayList<Square>();
+
+            while(getSquare(checkerX,mainY)!=null)
+            {
+                if(currentPlacement.contains(getSquare(checkerX,mainY)))
+                {
+                    count++;
+                    aroundSquare.add(getSquare(checkerX, mainY - 1));
+                    aroundSquare.add(getSquare(checkerX, mainY + 1));
+                    aroundSquare.add(getSquare(checkerX - 1, mainY));
+                    aroundSquare.add(getSquare(checkerX + 1, mainY));
+                    for(int i=0;i<aroundSquare.size();i++)
+                    {
+                        if(aroundSquare.get(i)!=null)
+                        {
+                            if(!currentPlacement.contains(aroundSquare.get(i)))
+                            {
+                                flagY = flagY || true;
+                            }
+                        }
+                        
+                    }
+                }
+                checkerX++;
+            }
+            if(count != currentPlacement.size() || !flagY)
+            {
+                restoreTileToPlayer();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
         else
         {
@@ -248,23 +346,15 @@ public class GameBoard
             startY = targetSquare.getPositionY();
             currentX = startX;
             currentY = startY;
-            while(currentX!=1&&!squareGrid.get((currentX-1)*15+(startY-1)).isEmpty())
-            {
-                currentX --;
-            }
-            while(currentY!=1&&!squareGrid.get((startX-1)*15+(currentY-1)).isEmpty())
+            while(currentX!=1&&!getSquare(startX, currentY).isEmpty())
             {
                 currentY --;
             }
-            if(currentX==1&&!squareGrid.get((currentX-1)*15+(startY-1)).isEmpty())
+            while(currentY!=1&&!getSquare(currentX, startY).isEmpty())
             {
-                wordPointerX = currentX;
+                currentX --;
             }
-            else
-            {
-                wordPointerX = currentX + 1;
-            }
-            if(currentY==1&&!squareGrid.get((startX-1)*15+(currentY-1)).isEmpty())
+            if(currentX==1&&!getSquare(startX, currentY).isEmpty())
             {
                 wordPointerY = currentY;
             }
@@ -272,29 +362,37 @@ public class GameBoard
             {
                 wordPointerY = currentY + 1;
             }
-            while(squareGrid.get((wordPointerX-1)*15+(startY-1)).isEmpty())
+            if(currentY==1&&!getSquare(currentX, startY).isEmpty())
+            {
+                wordPointerX = currentX;
+            }
+            else
+            {
+                wordPointerX = currentX + 1;
+            }
+            while(!getSquare(startX, wordPointerY).isEmpty())
             {
                
-               tmpConcatenatorX+=squareGrid.get((wordPointerX-1)*15+(startY-1)).peekTileOnSquare().getTileLetter();
-               newWords.add(squareGrid.get((wordPointerX-1)*15+(startY-1)));
-               wordPointerX++;
-            }
-            if(!wordList.contains(tmpConcatenatorX)&&tmpConcatenatorX.length()!=1)
-            {
-                wordList.add(tmpConcatenatorX);
-                words.add(new SquareCollection(newWords));
-            }
-            newWords = new ArrayList<Square>();
-            while(squareGrid.get((startX-1)*15+(wordPointerY-1)).isEmpty())
-            {
-               
-               tmpConcatenatorY+=squareGrid.get((startX-1)*15+(wordPointerY-1)).peekTileOnSquare().getTileLetter();
-               newWords.add(squareGrid.get((startX-1)*15+(wordPointerY-1)));
+               tmpConcatenatorY+=getSquare(startX, wordPointerY).peekTileOnSquare().getTileLetter();
+               newWords.add(getSquare(startX, wordPointerY));
                wordPointerY++;
             }
             if(!wordList.contains(tmpConcatenatorY)&&tmpConcatenatorY.length()!=1)
             {
                 wordList.add(tmpConcatenatorY);
+                words.add(new SquareCollection(newWords));
+            }
+            newWords = new ArrayList<Square>();
+            while(!getSquare(wordPointerX, startY).isEmpty())
+            {
+               
+               tmpConcatenatorX+=getSquare(wordPointerX, startY).peekTileOnSquare().getTileLetter();
+               newWords.add(getSquare(wordPointerX, startY));
+               wordPointerX++;
+            }
+            if(!wordList.contains(tmpConcatenatorX)&&tmpConcatenatorX.length()!=1)
+            {
+                wordList.add(tmpConcatenatorX);
                 words.add(new SquareCollection(newWords));
             }
 
